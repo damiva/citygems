2e306bb872b0132d5d2e4b6870da7c2ff9fc30cc
damiva <damiva@gmail.com>
2026-05-11 16:42:56 +0300

    contacts menu

---
+++ config.js
@@ -0,0 +1,19 @@
+const DIAMOND_CONFIG = {
+    translate: {
+        'RD': 'Круг',
+        'OV': 'Овал',
+        'PR': 'Принцесса',
+        'EM': 'Изумруд',
+        'PS': 'Груша',
+        'RAD': 'Радиант',
+        'CU': 'Кушон',
+        'MQ': 'Маркиз',
+        'AS': 'Ашер',
+        'HT': 'Сердце'
+    },
+    contacts: {
+        tg: "ZmFuY3lkaWFtcw==", 
+        wa: "Nzk4NTc3NzY2NTU="
+    },
+    itemsPerPage: 21
+};
