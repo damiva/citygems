e4d827a106fad68f2274d683ea7ee0d1e530fa5c
damiva <damiva@gmail.com>
2026-05-28 03:09:18 +0300

    гз

--- js/city.js
+++ js/city.js
@@ -1,1 +1,4 @@
-const somethingInteresting = ["dGc6Ly9yZXNvbHZlP2RvbWFpbj1jaXR5Z2Vtcw==", "bWFpbHRvOmluZm9AY2l0eWdlbXMucnU="];
+window.addEventListener("load", () => {
+    document.getElementById("tl").dataset.link = "dGc6Ly9yZXNvbHZlP2RvbWFpbj1jaXR5Z2Vtcw==";
+    document.getElementById("ml").dataset.link = "bWFpbHRvOmluZm9AY2l0eWdlbXMucnU=";
+});
