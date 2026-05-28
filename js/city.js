ce8b4c1fd62333ccbb4fd6ca6c0a38693576d979
damiva <damiva@gmail.com>
2026-05-28 15:55:01 +0300

    yp

--- js/city.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener("load", () => {
-    document.getElementById("tl").dataset.link = "dGc6Ly9yZXNvbHZlP2RvbWFpbj1jaXR5Z2Vtcw==";
-    document.getElementById("ml").dataset.link = "bWFpbHRvOmluZm9AY2l0eWdlbXMucnU=";
-});
